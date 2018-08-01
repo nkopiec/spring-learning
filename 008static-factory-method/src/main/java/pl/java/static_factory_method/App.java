41bfb56506e1e73cc5e6afa85425073ae3bc89c4
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2018-08-01 17:52:55 +0200

    static factory method

---
+++ 008static-factory-method/src/main/java/pl/java/static_factory_method/App.java
@@ -0,0 +1,22 @@
+package pl.java.static_factory_method;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import org.omg.CORBA.portable.ApplicationException;
+
+public class App {
+    public static void main( String[] args ) {
+   // 	Locale locale = Locale.getDefault();
+   //   Calendar calendar = GregorianCalendar.getInstance(locale);
+   //    
+   //    DateFormat formatter = SimpleDateFormat.getInstance();
+       
+   //    System.out.println(formatter.format(calendar.getTime()));
+    
+    ApplicationContext context
+    }
+}
