95043e201b1a778a652c390cdfd34f7baae5fc94
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2018-08-24 14:35:31 +0200

    change parameter responsible for automatic generate database, add entity, save object example inside transaction, automatic generate identifiers.

---
+++ 038first-entity/src/domain/Employee.java
@@ -0,0 +1,48 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String firstname;
+	private String lastName;
+	private double salary;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
---
+++ 038first-entity/src/pl/java/Main.java
@@ -0,0 +1,48 @@
+package pl.java;
+
+import javax.persistence.EntityManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+	
+		Employee employee = new Employee();
+		//employee.setId(1);
+		employee.setFirstname("Stefan");
+		employee.setLastName("Kowalski");
+		employee.setSalary(1530.34);
+		
+		Employee employee2 = new Employee();
+		employee2.setFirstname("Edward");
+		employee2.setLastName("Wolski");
+		employee2.setSalary(2534.34);
+		
+		Employee employee3 = new Employee();
+		employee3.setFirstname("Jakub");
+		employee3.setLastName("Kozak");
+		employee3.setSalary(432.87);
+		
+		Employee employee4 = new Employee();
+		employee4.setFirstname("S³awomir");
+		employee4.setLastName("Mrówka");
+		employee4.setSalary(2341.88);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(employee);
+		entityManager.persist(employee2);
+		entityManager.persist(employee3);
+		entityManager.persist(employee4);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
