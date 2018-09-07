c78147bf875a3af52e69872096e0e7643f1a81d4
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2018-09-07 16:24:22 +0200

    My own program

---
+++ 062fetch2/src/domain/Doctor.java
@@ -0,0 +1,17 @@
+package domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Doctor extends Person {
+	private double salary;
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
---
+++ 062fetch2/src/domain/Person.java
@@ -0,0 +1,47 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Person {
+	@Id
+	@GeneratedValue
+	public long id;
+	
+	public String name;
+	public String surname;
+	public int age;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
