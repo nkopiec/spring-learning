54d5f5c4d5058c32675e14a3134ce5c359d04a11
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2018-11-06 15:53:54 +0100

    angular JS

---
+++ 081angularJS/src/main/java/pl/java/service/UserService.java
@@ -0,0 +1,27 @@
+package pl.java.service;
+
+import java.util.List;
+
+import pl.java.model.User;
+ 
+ 
+ 
+public interface UserService {
+     
+    User findById(long id);
+     
+    User findByName(String name);
+     
+    void saveUser(User user);
+     
+    void updateUser(User user);
+     
+    void deleteUserById(long id);
+ 
+    List<User> findAllUsers(); 
+     
+    void deleteAllUsers();
+     
+    public boolean isUserExist(User user);
+     
+}
