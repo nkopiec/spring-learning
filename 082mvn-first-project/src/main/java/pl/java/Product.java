360337ecee226292298f5d300c571d9a4fe24059
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2018-11-15 19:26:35 +0100

    MVN

---
+++ 082mvn-first-project/src/main/java/pl/java/Product.java
@@ -0,0 +1,29 @@
+package pl.java;
+
+public class Product {
+	private String productName;
+	private double productPrice;
+	
+	
+	public Product(String productName, double productPrice) {
+		super();
+		this.productName = productName;
+		this.productPrice = productPrice;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	
+
+	
+}
---
+++ 082mvn-first-project/src/main/java/pl/java/ProductsControllers.java
@@ -0,0 +1,33 @@
+package pl.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("product")
+@RestController
+public class ProductsControllers {
+//	@RequestMapping(value = "get-product", method = RequestMethod.POST)
+	@PostMapping("get-product") //zamiennik 
+	public @ResponseBody List<Product> getProduct() {
+		List<Product> result = new ArrayList<>();
+		
+		result.add(new Product("Book ", 23.5));
+		result.add(new Product("Pen ", 1.8));
+		result.add(new Product("Coat ", 256.0));
+		
+		return result;
+	}
+//	@RequestMapping(value = "find-any", method = RequestMethod.GET)
+	@GetMapping("find-any") //zamiennik
+	public Product findAny() {
+		return new Product("Book ", 23.5);
+	}
+}
